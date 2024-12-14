95ff8c853c0ea02195c5011345d140b211b0dbe1
hwan2-99 <thh1233123@gmail.com>
2024-12-14 11:33:37 +0900

    [feat] 스케쥴 Entity 클래스 구현

--- src/main/java/com/mindgoal/domain/schedule/entity/Schedule.java
+++ src/main/java/com/mindgoal/domain/schedule/entity/Schedule.java
@@ -1,1 +1,39 @@
-package com.mindgoal.domain.schedule.entity; 
+package com.mindgoal.domain.schedule.entity;
+
+import com.mindgoal.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "SCHEDULES")
+@Entity
+@Getter
+public class Schedule extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false)
+    private Long id;
+
+    @Column(name = "SESSION_COUNT")
+    private int sessionCount;
+
+    @Column(name = "START_DATE", nullable = false)
+    private LocalDate startDate;
+
+    @Column(name = "END_DATE", nullable = false)
+    private LocalDate endDate;
+
+    @Column(name = "SESSION_TYPE")
+    private String sessionType;
+
+    @Column(name = "STATUS")
+    private String status;
+}
