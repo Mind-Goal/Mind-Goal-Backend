3193591b8b5dc608e20c49ad4633536afaf92000
duhyeonkwak-korea <duhyeonkwak@gmail.com>
2025-01-04 12:48:56 +0900

    [ci] Kakao로그인, JWT토큰 로직 병합

--- src/main/java/com/mindgoal/config/jwt/JwtTokenProvider.java
+++ src/main/java/com/mindgoal/config/jwt/JwtTokenProvider.java
@@ -4,11 +4,11 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.security.core.Authentication;
-import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 
@@ -16,17 +16,19 @@ import java.util.Date;
 @Component
 public class JwtTokenProvider {
 
-    @Value("${}") String secretkey;
+    private final Key key;
     private final long VALID_MILISECOND = 1000L * 60 * 60; // 1시간
+    private static final long REFRESH_TOKEN_VALIDITY = 1000L * 60 * 60 * 24; // 24시간
+    private static final String BEARER_PREFIX = "Bearer ";
 
-    private Key getSecretKey(@Value("${}") String secretkey){
-        byte[] keyBytes = secretkey.getBytes(StandardCharsets.UTF_8);
-        return Keys.hmacShaKeyFor(keyBytes);
+    public JwtTokenProvider(@Value("${jwt.secret}") String secretkey) {
+        byte[] keyBytes = Decoders.BASE64.decode(secretkey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
     }
 
-    private String getUserName(String jwtToken){
+    public String getUserName(String jwtToken){
         return Jwts.parserBuilder()
-                .setSigningKey(secretkey)
+                .setSigningKey(key)
                 .build()
                 .parseClaimsJws(jwtToken)
                 .getBody()
@@ -36,9 +38,9 @@ public class JwtTokenProvider {
     public boolean valideToken(String jwtToken){
         try{
             Jws<Claims> claims = Jwts.parserBuilder()
-                                    .setSigningKey(getSecretKey(secretkey))
-                                    .build()
-                                    .parseClaimsJws(jwtToken);
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(jwtToken);
             return !claims.getBody().getExpiration().before(new Date());
         }catch (Exception e){
             return false;
@@ -47,16 +49,33 @@ public class JwtTokenProvider {
 
     public Authentication getAuthentication(String jwtToken){
         //유저 데이터 가져와서 비교
-        return
+        return null;
     }
 
+    private Claims createClaims(String userName){
+        Claims claims = Jwts.claims();
+        claims.setSubject(userName);
+        claims.setIssuedAt(new Date());
 
-    public String generateToken(String username){
-        return Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(new Date().getTime() + VALID_MILISECOND))
-                .signWith(getSecretKey(secretkey), SignatureAlgorithm.HS256)
+        return claims;
+    }
+
+    public JwTokenDto generateToken(String userName){
+        Claims claims = createClaims(userName);
+        long now = (new Date()).getTime();
+        Date accessTokenExpiresIn = new Date(now + VALID_MILISECOND);
+
+        String accessToken = Jwts.builder()
+                .setClaims(claims) // 발행 유저 정보 저장
+                .setExpiration(accessTokenExpiresIn)
+                .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
+
+        String refreshToken = Jwts.builder()
+                .setExpiration(new Date(now + REFRESH_TOKEN_VALIDITY))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+
+        return new JwTokenDto(BEARER_PREFIX, accessToken, refreshToken, accessTokenExpiresIn);
     }
 }
