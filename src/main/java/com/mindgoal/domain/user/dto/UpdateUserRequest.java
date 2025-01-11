6ebc91458ef4119b9b4e7eb8a733b0b0a82cc314
JJangcoding <bymyself103@naver.com>
2025-01-11 13:48:11 +0900

    [feat]: UpdateUserRequest DTO 수정

--- src/main/java/com/mindgoal/domain/user/dto/UpdateUserRequest.java
+++ src/main/java/com/mindgoal/domain/user/dto/UpdateUserRequest.java
@@ -1,6 +1,7 @@
 package com.mindgoal.domain.user.dto;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,4 +10,10 @@ import lombok.NoArgsConstructor;
 public class UpdateUserRequest {
     private String name;
     private String profileImage;
+
+    @Builder
+    public UpdateUserRequest(String name, String profileImage) {
+        this.name = name;
+        this.profileImage = profileImage;
+    }
 }
