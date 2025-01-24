18576ac43ba1836c96e6c4a887c5fd4966afdf11
hwan2-99 <thh1233123@gmail.com>
2025-01-24 18:02:58 +0900

    [feat] 채팅과 관련된 레포지토리 인터페이스 구현

---
+++ src/main/java/com/mindgoal/domain/chat/repository/ChatRoomRepository.java
@@ -0,0 +1,7 @@
+package com.mindgoal.domain.chat.repository;
+
+import com.mindgoal.domain.chat.entity.ChatRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+}
