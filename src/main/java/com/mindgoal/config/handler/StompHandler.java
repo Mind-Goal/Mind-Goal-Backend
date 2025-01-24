8b2727e7f4f9cdec42be3b965d6e7db1d372809a
hwan2-99 <thh1233123@gmail.com>
2025-01-24 17:54:55 +0900

    [feat] 채팅방 생성과 메시지를 위한 웹소켓 의존성 및 핸들러 클래스 구현

---
+++ src/main/java/com/mindgoal/config/handler/StompHandler.java
@@ -0,0 +1,33 @@
+package com.mindgoal.config.handler;
+
+import java.util.Objects;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class StompHandler implements ChannelInterceptor {
+
+    @Override
+    public void postSend(Message<?> message, MessageChannel channel, boolean sent) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        String sessionId = accessor.getSessionId();
+
+        switch (Objects.requireNonNull(accessor.getCommand())) {
+            case CONNECT -> log.info("CONNECT: " + message);
+            case CONNECTED -> log.info("CONNECTED: " + message);
+            case DISCONNECT -> log.info("DISCONNECT: " + message);
+            case SUBSCRIBE -> log.info("SUBSCRIBE: " + message);
+            case UNSUBSCRIBE -> log.info("UNSUBSCRIBE: " + sessionId);
+            case SEND -> log.info("SEND: " + sessionId);
+            case MESSAGE -> log.info("MESSAGE: " + sessionId);
+            case ERROR -> log.info("ERROR: " + sessionId);
+            default -> log.info("UNKNOWN: " + sessionId);
+        }
+
+    }
+}
