eb053ad311ae7160d1ad47973b4a5dd458eec8d8
hwan2-99 <thh1233123@gmail.com>
2025-01-31 17:36:26 +0900

    [feat] 현재 사용자의 정보를 관리하기 위한 jwt 핸들러 및 유저정보 엔티티 구현

---
+++ src/main/java/com/mindgoal/domain/user/entity/auth/PrincipalDetails.java
@@ -0,0 +1,67 @@
+package com.mindgoal.domain.user.entity.auth;
+
+import com.mindgoal.domain.user.entity.User;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@RequiredArgsConstructor
+public class PrincipalDetails implements UserDetails /*, OAuth2User*/ {
+    private final User user;
+
+    public Long getId() {
+        return user.getId();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(user.getEmail()));
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail(); // 이메일로 로그인, 이메일로 jwt payload 생성
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    // @AuthenticationPrincipal 사용을 위한 implement, jwt 인증에선 사용하지 않음 (email로 인증)
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
