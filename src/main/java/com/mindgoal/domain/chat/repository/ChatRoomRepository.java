c2d5a1926e40c9d0439e5150a1ff576e6be5582c
hwan2-99 <thh1233123@gmail.com>
2025-02-10 20:39:26 +0900

    [feat] 채팅방 유무 확인 메서드 변경

--- src/main/java/com/mindgoal/domain/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/mindgoal/domain/chat/repository/ChatRoomRepository.java
@@ -2,12 +2,13 @@ package com.mindgoal.domain.chat.repository;
 
 import com.mindgoal.domain.chat.entity.ChatRoom;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-    boolean existsByExpertIdAndUserId(Long expertId, Long userId);
-
     List<ChatRoom> findAllByExpertId(Long expertId);
 
     List<ChatRoom> findAllByUserId(Long userId);
+
+    Optional<ChatRoom> findByExpertIdAndUserId(Long expertId, Long userId);
 }
