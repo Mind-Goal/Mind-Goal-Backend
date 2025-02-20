00e4ae665079f2422b5a4c9c2ca5f1f01e68f98f
hwan2-99 <thh1233123@gmail.com>
2025-02-20 21:56:43 +0900

    [test] 어플리케이션 테스트를위한 클래스 구현

--- src/test/java/com/mindgoal/MindGoalBackendApplicationTest.java
+++ src/test/java/com/mindgoal/MindGoalBackendApplicationTest.java
@@ -2,8 +2,10 @@ package com.mindgoal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MindGoalBackendApplicationTest {
     @Test
     void contextLoads() {
