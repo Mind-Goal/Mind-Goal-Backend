756b123c2abf2bb4687df87341ba466ed4b1c4a3
hwan2-99 <thh1233123@gmail.com>
2025-02-20 18:08:53 +0900

    [test] test 메인클래스 구현

---
+++ src/test/java/com/mindgoal/MindGoalBackendApplicationTest.java
@@ -0,0 +1,11 @@
+package com.mindgoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindGoalBackendApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
