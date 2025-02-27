69614669f6878a130cba492c43bd523fe9b61679
hwan2-99 <thh1233123@gmail.com>
2025-02-27 16:56:48 +0900

    [refactor] 미사용 선언문삭제

--- src/main/java/com/mindgoal/domain/chat/service/ChatService.java
+++ src/main/java/com/mindgoal/domain/chat/service/ChatService.java
@@ -2,15 +2,10 @@ package com.mindgoal.domain.chat.service;
 
 import com.mindgoal.domain.chat.dto.ChatMessageRequest;
 import com.mindgoal.domain.chat.dto.ChatMessageResponse;
-import com.mindgoal.domain.chat.dto.ChatRoomRequest;
-import com.mindgoal.domain.chat.dto.ChatRoomResponse;
 import com.mindgoal.domain.chat.entity.ChatMessage;
 import com.mindgoal.domain.chat.entity.ChatRoom;
-import com.mindgoal.domain.chat.entity.ChatRoomStatus;
 import com.mindgoal.domain.chat.repository.ChatMessageRepository;
 import com.mindgoal.domain.chat.repository.ChatRoomRepository;
-import com.mindgoal.domain.expert.repository.ExpertRepository;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,7 +15,6 @@ import org.springframework.transaction.annotation.Transactional;
 public class ChatService {
     private final ChatRoomRepository chatRoomRepository;
     private final ChatMessageRepository chatMessageRepository;
-    private final ExpertRepository expertRepository;
 
     @Transactional
     public ChatMessageResponse sendMessage(final Long userId, final ChatMessageRequest chatMessageRequest) {
