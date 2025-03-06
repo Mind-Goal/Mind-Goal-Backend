fffffc3a2d09be4df048d9feba9b2346158ba284
hwan2-99 <thh1233123@gmail.com>
2025-03-06 23:54:44 +0900

    [refactor] 서비스 클래스 트랜잭션 추가

--- src/main/java/com/mindgoal/domain/schedule/service/ScheduleService.java
+++ src/main/java/com/mindgoal/domain/schedule/service/ScheduleService.java
@@ -9,9 +9,11 @@ import com.mindgoal.domain.schedule.repository.ScheduleRepository;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ScheduleService {
     private final ScheduleRepository scheduleRepository;
 
