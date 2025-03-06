fc280a023822c0770bbab4912e95bf2c4d11353e
hwan2-99 <thh1233123@gmail.com>
2025-03-06 23:35:46 +0900

    [refactor] 일정상태 관리를 위한 enum 클래스 필드 추가

--- src/main/java/com/mindgoal/domain/schedule/entity/ScheduleStatus.java
+++ src/main/java/com/mindgoal/domain/schedule/entity/ScheduleStatus.java
@@ -1,7 +1,13 @@
 package com.mindgoal.domain.schedule.entity;
 
 public enum ScheduleStatus {
-    AVAILABLE,
-    BOOKED,
-    CANCELLED;
+    AVAILABLE("예약가능"),
+    BOOKED("예약됨"),
+    CANCELLED("취소");
+
+    private final String description;
+
+    ScheduleStatus(String description) {
+        this.description = description;
+    }
 }
