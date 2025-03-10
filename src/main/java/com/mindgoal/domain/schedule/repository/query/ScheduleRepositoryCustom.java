47a85d4b03b8119b1636ed3295c3cadedf254743
hwan2-99 <thh1233123@gmail.com>
2025-03-10 21:51:59 +0900

    [feat] 달별 일정조회 기능 구현

--- src/main/java/com/mindgoal/domain/schedule/repository/query/ScheduleRepositoryCustom.java
+++ src/main/java/com/mindgoal/domain/schedule/repository/query/ScheduleRepositoryCustom.java
@@ -1,7 +1,13 @@
 package com.mindgoal.domain.schedule.repository.query;
 
+import com.mindgoal.domain.schedule.entity.Schedule;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public interface ScheduleRepositoryCustom {
     boolean existSchedule(LocalDateTime startTime, LocalDateTime endTime, Long userId, Long expertId);
+    List<Schedule> findAllByDateAndUserId(LocalDate startTime, LocalDate endTime, Long userId);
+
+    List<Schedule> findAllByDateAndExpertId(LocalDate startTime, LocalDate endTime, Long expertId);
 }
--- src/main/java/com/mindgoal/domain/schedule/service/ScheduleService.java
+++ src/main/java/com/mindgoal/domain/schedule/service/ScheduleService.java
@@ -1,12 +1,15 @@
 package com.mindgoal.domain.schedule.service;
 
+import com.mindgoal.domain.expert.repository.ExpertRepository;
 import com.mindgoal.domain.schedule.dto.ScheduleRequest;
 import com.mindgoal.domain.schedule.dto.ScheduleResponse;
 import com.mindgoal.domain.schedule.entity.Schedule;
 import com.mindgoal.domain.schedule.entity.ScheduleDate;
 import com.mindgoal.domain.schedule.entity.SessionType;
 import com.mindgoal.domain.schedule.repository.ScheduleRepository;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,15 +19,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class ScheduleService {
     private final ScheduleRepository scheduleRepository;
+    private final ExpertRepository expertRepository;
 
     public ScheduleResponse create(final ScheduleRequest request, final Long userId) {
-        SessionType sessionType = SessionType.from(request.sessionType());
+        final SessionType sessionType = SessionType.from(request.sessionType());
         final LocalDateTime startTime = request.startTime();
         final LocalDateTime endTime = sessionType.calculateEndTime(request.startTime());
 
         validateExist(startTime, endTime, userId, request.expertId());
-        ScheduleDate scheduleDate = new ScheduleDate(startTime, endTime);
-        Schedule schedule = new Schedule(userId, request.expertId(), sessionType.getName(), scheduleDate);
+        final ScheduleDate scheduleDate = new ScheduleDate(startTime, endTime);
+        final Schedule schedule = new Schedule(userId, request.expertId(), sessionType.getName(), scheduleDate);
 
         return ScheduleResponse.from(scheduleRepository.save(schedule));
     }
@@ -39,5 +43,22 @@ public class ScheduleService {
     private boolean existSchedule(final LocalDateTime startTime, final LocalDateTime endTime, final Long userId,
                                   final Long expertId) {
         return scheduleRepository.existSchedule(startTime, endTime, userId, expertId);
+    }
+
+    public List<ScheduleResponse> findMySchedules(final LocalDate startTime, final LocalDate endTime,
+                                                  final Long userId) {
+        final List<Schedule> schedules = findScheduleByUserType(startTime, endTime, userId);
+        return schedules.stream()
+                .map(ScheduleResponse::from)
+                .toList();
+    }
+
+    private List<Schedule> findScheduleByUserType(final LocalDate startTime, final LocalDate endTime,
+                                                  final Long userId) {
+        if (expertRepository.existsByUserId(userId)) {
+            final Long expertId = expertRepository.findExpertByUserId(userId).getId();
+            return scheduleRepository.findAllByDateAndExpertId(startTime, endTime, expertId);
+        }
+        return scheduleRepository.findAllByDateAndUserId(startTime, endTime, userId);
     }
 }
